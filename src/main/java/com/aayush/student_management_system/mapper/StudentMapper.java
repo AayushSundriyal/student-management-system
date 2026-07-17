3620dbdab8be491dc9ece51270582e99116b91b9
Aayush Sundriyal <aayushsundriyal111@gmail.com>
2026-07-17 18:32:48 +0530

    Initial commit

---
+++ src/main/java/com/aayush/student_management_system/mapper/StudentMapper.java
@@ -0,0 +1,25 @@
+package com.aayush.student_management_system.mapper;
+
+import com.aayush.student_management_system.dto.StudentDto;
+import com.aayush.student_management_system.entity.Student;
+
+public class StudentMapper {
+    public static StudentDto mapToStudentDto(Student student){
+        StudentDto studentDto = new StudentDto(
+                student.getId(),
+                student.getFirstName(),
+                student.getLastName(),
+                student.getEmail()
+        );
+        return studentDto;
+    }
+    public static Student mapToStudent(StudentDto studentDto){
+        Student student = new Student(
+            studentDto.getId(),
+            studentDto.getFirstName(),
+            studentDto.getLastName(),
+            studentDto.getEmail()
+        );
+        return student;
+    }
+}
---
+++ src/main/java/com/aayush/student_management_system/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.aayush.student_management_system.service;
+
+import com.aayush.student_management_system.dto.StudentDto;
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+public interface StudentService {
+    List<StudentDto> getAllStudents();
+
+    void createStudent(StudentDto student);
+
+    StudentDto getStudentById(Long studentId);
+
+    void updateStudent(@Valid StudentDto studentDto);
+
+    void deleteStudent(Long studentId);
+}
